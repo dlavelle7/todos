c335c308f89d6e63ef221b119ccfde1070f5dbe0
David Lavelle <d.lavelle7@yahoo.ie>
2015-11-27 09:43:41 +0000

    Some progress with todo example.

--- js/application.js
+++ js/application.js
@@ -1,1 +1,4 @@
-window.Todos = Ember.Application.create()
+window.Todos = Ember.Application.create();
+
+// Use fixture adapter (browser memory cache)
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
